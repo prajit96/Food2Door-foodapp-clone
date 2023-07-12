200a1fdeaa706f382b872829bd5ede8888ea547b
prajit96 <santraprajit@gmail.com>
2023-07-13 00:15:52 +0530

    navbar done

---
+++ food_app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import NavBar from './Components/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+    </div>
+  );
+}
+
+export default App;
